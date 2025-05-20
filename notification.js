c406e9840840d64ae26480efcd119cfb500ad8fd
Onur Şavkı <42342278+onursavki@users.noreply.github.com>
2025-05-20 23:41:09 +0300

    notification.js

--- notification.js
+++ notification.js
@@ -1,21 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const notifications = [
         "📧 1 Davet Mektubunuz Var.<br/><a href='https://kick.com/ekipgamestv' target='_blank'>kick.com/EkipGamesTV</a>",
-        "Discord'a gelmeyi unutmayın.<br/><a href='https://discord.gg/Scsxdnv3xw' target='_blank'>Ekip Games™</a>",
-        "Man Cityliner - v1.54<br/><a href='https://www.ekipgames.com/p/neoplan-cityliner.html' target='_blank'>Mod'u İndir</a>",
-        "Neoplan Tourliner C13 - v1.54<br/><a href='https://www.ekipgames.com/p/neoplan-tourliner-c13.html' target='_blank'>Mod'u İndir</a>",
-        "Man Lion's 2023 - v1.54<br/><a href='https://www.ekipgames.com/p/yeni-man-lions.html' target='_blank'>Mod'u İndir</a>",
-        "Yeni Tourismo 16RHD 2020 - v1.54<br/><a href='https://www.ekipgames.com/p/yeni-tourismo-16rhd-2020.html' target='_blank'>Mod'u İndir</a>",
-        "Travego SE 15SHD - v1.54<br/><a href='https://www.ekipgames.com/p/travego-se-15shd.html' target='_blank'>Mod'u İndir</a>",
-        "Travego SE 17SHD - v1.54<br/><a href='https://www.ekipgames.com/p/travego-se-17shd.html' target='_blank'>Mod'u İndir</a>",
-        "2023 Travego 15SHD - v1.54<br/><a href='https://www.ekipgames.com/p/2023-travego-15shd.html' target='_blank'>Mod'u İndir</a>",
-        "Setra S531DT - v1.54<br/><a href='https://www.ekipgames.com/p/setra-s531dt.html' target='_blank'>Mod'u İndir</a>",
-        "Temsa Safir 2 VIP - v1.54<br/><a href='https://www.ekipgames.com/p/temsa-safir-2-vip.html#google_vignette' target='_blank'>Mod'u İndir</a>",
-        "Temsa Safir Plus HD 13 - v1.54<br/><a href='https://www.ekipgames.com/p/temsa-safir-plus-hd-13.html' target='_blank'>Mod'u İndir</a>",
-        "Temsa Maraton 3 VIP - v1.54<br/><a href='https://www.ekipgames.com/p/temsa-maraton-3-vip.html' target='_blank'>Mod'u İndir</a>",
-        "Grand Utopia 1:1 - v1.54<br/><a href='https://www.ekipgames.com/p/grand-utopia.html' target='_blank'>Mod'u İndir</a>",
-        "OyuncuyusBisMap - v1.54<br/><a href='https://www.ekipgames.com/p/turkiye-yks-haritasi.html' target='_blank'>Mod'u İndir</a>",
-        "ATS ve ETS2 Kış Modu<br/><a href='https://www.ekipgames.com/p/ats-ve-ets2-kis-modu.html' target='_blank'>Mod'u İndir</a>",
+        "🎮 1 Yeni Oyun Yayınlandı.<br/><a href='https://www.ekipgames.com/2025/02/RoadCraft.html' target='_blank'>RoadCraft</a>",
     ];
 
     let currentIndex = parseInt(localStorage.getItem('currentNotificationIndex')) || 0;
