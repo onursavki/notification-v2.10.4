1c64f304b90c481bf43176eb310cef6cd254fb2f
Onur Şavkı <42342278+onursavki@users.noreply.github.com>
2025-06-11 09:39:05 +0300

    notification.js

--- notification.js
+++ notification.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const notifications = [
         "📧 Davet Mektubunuz Var.<br/><a href='https://kick.com/ekipgamestv' target='_blank'>kick.com/EkipGamesTV</a>",
         "Yeni Oyun Yayınlandı.<br/><a href='https://www.ekipgames.com/2025/02/RoadCraft.html' target='_blank'>RoadCraft</a>",
-        " Yeni Otobüs Yayınlandı.<br/><a href='https://www.ekipgames.com/p/setra-s516hd.html' target='_blank'>Setra S516HD</a>",
         " 💰 Web Sitemizi Destekle.<br/><a href='https://donate.bynogame.com/ekipgamestv' target='_blank'>ByNoGame</a>",
     ];
 
